571de04b0888a0372dd02c26504e155d9ec44352
Mhel DC <romeldelacruz@gmail.com>
2017-02-09 00:29:44 +0800

    Populated rooms, room rates and room types table

--- assets/db/careserve.sql
+++ assets/db/careserve.sql
@@ -16,6 +16,31 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`careserve` /*!40100 DEFAULT CHARACTER S
 
 USE `careserve`;
 
+/*Table structure for table `ca_booking` */
+
+DROP TABLE IF EXISTS `ca_booking`;
+
+CREATE TABLE `ca_booking` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `bsource` int(1) NOT NULL DEFAULT '0',
+  `bsrcid` bigint(20) NOT NULL DEFAULT '0',
+  `bid` varchar(25) NOT NULL DEFAULT 'xxx',
+  `guestid` bigint(20) NOT NULL DEFAULT '0',
+  `rmtypeid` bigint(20) NOT NULL DEFAULT '0',
+  `rmid` bigint(20) NOT NULL DEFAULT '0',
+  `overnight` bit(1) NOT NULL DEFAULT b'0',
+  `guestcount` int(11) NOT NULL DEFAULT '1',
+  `adultshare` int(11) NOT NULL DEFAULT '1',
+  `childshare` int(11) NOT NULL DEFAULT '1',
+  `createdbyid` bigint(20) NOT NULL DEFAULT '0',
+  `datecreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modifiedbyid` bigint(20) NOT NULL DEFAULT '0',
+  `datemodified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`,`bsrcid`,`bid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `ca_booking` */
+
 /*Table structure for table `ca_guest_info` */
 
 DROP TABLE IF EXISTS `ca_guest_info`;
@@ -38,6 +63,8 @@ CREATE TABLE `ca_guest_info` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+/*Data for the table `ca_guest_info` */
+
 /*Table structure for table `ca_inv_audit` */
 
 DROP TABLE IF EXISTS `ca_inv_audit`;
@@ -53,6 +80,8 @@ CREATE TABLE `ca_inv_audit` (
   `datemodified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`,`itemid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `ca_inv_audit` */
 
 /*Table structure for table `ca_inv_items` */
 
@@ -70,6 +99,8 @@ CREATE TABLE `ca_inv_items` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+/*Data for the table `ca_inv_items` */
+
 /*Table structure for table `ca_queue_served` */
 
 DROP TABLE IF EXISTS `ca_queue_served`;
@@ -86,6 +117,8 @@ CREATE TABLE `ca_queue_served` (
   `datemodified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`,`bid`,`guestid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `ca_queue_served` */
 
 /*Table structure for table `ca_reservation` */
 
@@ -108,6 +141,8 @@ CREATE TABLE `ca_reservation` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+/*Data for the table `ca_reservation` */
+
 /*Table structure for table `ca_reservesetting` */
 
 DROP TABLE IF EXISTS `ca_reservesetting`;
@@ -127,6 +162,8 @@ CREATE TABLE `ca_reservesetting` (
   `datemodified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `ca_reservesetting` */
 
 /*Table structure for table `ca_room_rates` */
 
@@ -148,7 +185,11 @@ CREATE TABLE `ca_room_rates` (
   `datemodified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modifiedbyid` bigint(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`,`rmtypeid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `ca_room_rates` */
+
+insert  into `ca_room_rates`(`id`,`rmtypeid`,`regular`,`regtimein`,`regtimeout`,`overnight`,`ovntimein`,`ovntimeout`,`extension`,`extratebase`,`datecreated`,`createdbyid`,`datemodified`,`modifiedbyid`) values (1,1,'1000.00','07:00:00','17:00:00','1000.00','19:00:00','05:00:00','1000.00',1,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(2,2,'1500.00','07:00:00','17:00:00','1500.00','19:00:00','05:00:00','1500.00',1,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(3,3,'2000.00','07:00:00','17:00:00','2000.00','19:00:00','05:00:00','2000.00',1,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(4,4,'2500.00','07:00:00','17:00:00','2500.00','19:00:00','05:00:00','2500.00',1,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0);
 
 /*Table structure for table `ca_rooms` */
 
@@ -165,7 +206,11 @@ CREATE TABLE `ca_rooms` (
   `datemodified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modifiedbyid` bigint(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+
+/*Data for the table `ca_rooms` */
+
+insert  into `ca_rooms`(`id`,`typeid`,`roomcode`,`roomdesc`,`capacity`,`datecreated`,`createdbyid`,`datemodified`,`modifiedbyid`) values (1,1,'UM01','Umbrella 1',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(2,1,'GA01','Gazebo 1',15,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(3,1,'CA01','Cabanas 1',20,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(4,2,'ST01','Standard Room 1',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(5,2,'ST02','Standard Room 2',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(6,2,'ST03','Standard Room 3',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(7,2,'ST04','Standard Room 4',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(8,2,'ST05','Standard Room 5',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(9,2,'ST06','Standard Room 6',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(10,2,'ST07','Standard Room 7',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(11,2,'ST08','Standard Room 8',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(12,2,'FA01','Family Room 1',20,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(13,2,'FA02','Family Room 2',20,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(14,3,'VI01','Villa 1',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(15,3,'VI02','Villa 2',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(16,3,'VI03','Villa 3',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(17,3,'VI04','Villa 4',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(18,3,'VI05','Villa 5 (Open Villa)',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(19,3,'VI06','Villa 6',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(20,4,'FH01','Function Hall 1',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0),(21,4,'PV01','Pavillon 1',10,'2017-02-09 00:06:32',0,'2017-02-09 00:06:32',0);
 
 /*Table structure for table `ca_roomtype` */
 
@@ -181,7 +226,11 @@ CREATE TABLE `ca_roomtype` (
   `datemodified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modifiedbyid` bigint(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `ca_roomtype` */
+
+insert  into `ca_roomtype`(`id`,`typedesc`,`dptype`,`dpvalue`,`datecreated`,`createdbyid`,`datemodified`,`modifiedbyid`) values (1,'Cottages','',0,'2017-02-09 00:06:03',0,'2017-02-09 00:06:03',0),(2,'Rooms','',0,'2017-02-09 00:06:03',0,'2017-02-09 00:06:03',0),(3,'Villa','',0,'2017-02-09 00:06:03',0,'2017-02-09 00:06:03',0),(4,'Function Hall','',0,'2017-02-09 00:06:03',0,'2017-02-09 00:06:03',0);
 
 /*Table structure for table `ca_user_roles` */
 
@@ -201,7 +250,11 @@ CREATE TABLE `ca_user_roles` (
   `datemodified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modifiedbyid` bigint(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `ca_user_roles` */
+
+insert  into `ca_user_roles`(`id`,`roledesc`,`allowlogin`,`allowbilling`,`allowreserve`,`allowinventory`,`allowcashier`,`allowsettings`,`datecreated`,`createdbyid`,`datemodified`,`modifiedbyid`) values (1,'CA-Administrator','','','','','','','2017-02-06 02:53:17',0,'2017-02-06 02:53:17',0);
 
 /*Table structure for table `ca_users` */
 
@@ -222,7 +275,11 @@ CREATE TABLE `ca_users` (
   `datemodified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modifiedbyid` bigint(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`,`firstname`,`lastname`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `ca_users` */
+
+insert  into `ca_users`(`id`,`barcodeid`,`loginid`,`loginpw`,`lastname`,`firstname`,`middlename`,`roleid`,`recstatus`,`datecreated`,`createdbyid`,`datemodified`,`modifiedbyid`) values (1,'0000000001','ca-admin','ccc1c1812d2258fb841a8e78ff7899a0','Admin','Costa-Abril','',1,'','2017-02-06 02:55:36',0,'2017-02-06 02:55:36',0);
 
 /*Table structure for table `ci_sessions` */
 
@@ -236,6 +293,10 @@ CREATE TABLE `ci_sessions` (
   PRIMARY KEY (`id`),
   KEY `ci_sessions_timestamp` (`timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `ci_sessions` */
+
+insert  into `ci_sessions`(`id`,`ip_address`,`timestamp`,`data`) values ('41h560s4rlal8mkmc08d7agvn0ana3ol','127.0.0.1',1486325069,'__ci_last_regenerate|i:1486325069;'),('bumusqqonos4e6h8d60pe1pqed8i141o','::1',1486325118,'__ci_last_regenerate|i:1486324870;init|s:29:\"This is just an intialisation\";'),('c9t927uq1rpr3796qvccelff7ts5nnnp','::1',1486321593,'__ci_last_regenerate|i:1486321593;init|s:29:\"This is just an intialisation\";'),('i0sqoprr8hqrvqese9foalvjsl84khts','127.0.0.1',1486325004,'__ci_last_regenerate|i:1486325004;'),('i1opsn96gi9qnge3skeop5i3h6lommuk','::1',1486223430,'__ci_last_regenerate|i:1486223430;init|s:29:\"This is just an intialisation\";'),('k0amvkqe5nc4b502f2er5uu86aeskgs2','::1',1486325452,'__ci_last_regenerate|i:1486325181;init|s:29:\"This is just an intialisation\";'),('l40r7mg8q18pbqodi20d8fk1hkstc72r','::1',1486324812,'__ci_last_regenerate|i:1486324538;init|s:29:\"This is just an intialisation\";'),('m6m6juuc21s4plh4eqvgvuq3a1dss1k8','::1',1486325593,'__ci_last_regenerate|i:1486325566;init|s:29:\"This is just an intialisation\";'),('metcv4o5jji98ak2msimnf597c9r7lmk','::1',1486383479,'__ci_last_regenerate|i:1486383477;init|s:29:\"This is just an intialisation\";'),('qa3tpceaam90iovs5puu194jbn9485pr','127.0.0.1',1486324871,'__ci_last_regenerate|i:1486324871;');
 
 /* Procedure structure for procedure `sp_tranusers` */
 
@@ -323,6 +384,30 @@ begin
 		select 'Nothing to do here.';
 	end if;
 end */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `sp_validatecredentials` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `sp_validatecredentials` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_validatecredentials`(lid varchar(25), lpw text)
+BEGIN
+		declare recordcount int;
+		
+		set recordcount = (select count(id) from `ca_users` where `loginid` = lid and `loginpw` = md5(lpw));
+		
+		if recordcount > 0 then
+			select	a.`id` as uid, a.`loginid` as lid, a.`lastname` as lname, a.`firstname` as fname,
+					b.`allowlogin`, b.`allowbilling`, b.`allowreserve`, b.`allowinventory`, b.`allowcashier`, b.`allowsettings`
+			from 			`ca_users` 		as a
+				inner join	`ca_user_roles`	as b on a.`roleid` = b.`id`
+			where a.`loginid` = lid
+			  and a.`loginpw` = md5(lpw);
+		end if;
+		
+    END */$$
 DELIMITER ;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
